a5fd0d46fcfbc760705d10a4b39f07c559e69f15
paul_alarcon <pa.f.e@hotmail.com>
2019-03-25 23:42:13 -0400

    Initial commit

---
+++ app/src/main/java/com/paul_alarcon/igapppaul/MainActivity.java
@@ -0,0 +1,69 @@
+package com.paul_alarcon.igapppaul;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "LoginActivity";
+
+    private EditText userInput;
+    private EditText passInput;
+    private Button btnInput;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        userInput = findViewById(R.id.userInput);
+        passInput = findViewById(R.id.passInput);
+        btnInput = findViewById(R.id.btnInput);
+
+        btnInput.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = userInput.getText().toString();
+                String password = passInput.getText().toString();
+                login(username, password);
+            }
+        });
+
+    }
+
+    private void login(String username, String password) {
+
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if( e != null){
+                    Log.e(TAG, "Issue with Login!");
+                    e.printStackTrace();
+                    return;
+                }
+                goMainActivity();
+                Log.e(TAG, "Signed in Successfully");
+                finish();
+            }
+        });
+    }
+
+    private void goMainActivity() {
+
+        Intent i = new Intent(this, HomeActivity.class);
+        startActivity(i);
+
+    }
+
+}
+
+
